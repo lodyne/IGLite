5008f30dc70994d39b881e149b9be7d78598d430
lodyne <lodgmtui@gmail.com>
2020-09-10 11:04:01 +0300

    modify models.py and post_list.html

--- app1/models.py
+++ app1/models.py
@@ -2,11 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    # title=models.CharField(max_length=100)
     caption=models.TextField()
-    # posts=models.FileField(upload_to='posts/images')
     posts=models.ImageField(upload_to='posts/images')
     
 
     def __str__(self):
-        return self.title
+        return self.caption
