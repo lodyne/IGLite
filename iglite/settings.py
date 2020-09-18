b4b1ddb089190912c0818dc4e662eab9ec01e60c
lodyne <lodgmtui@gmail.com>
2020-09-18 17:50:49 +0300

    added users app and fixing the layouts

--- iglite/settings.py
+++ iglite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'insta.apps.InstaConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
