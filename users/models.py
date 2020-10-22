669658c36ba6fc92d3a97a587aac441d8bfd784b
lodyne <lodgmtui@gmail.com>
2020-10-22 12:41:18 +0300

    deal with default profile pic

--- users/models.py
+++ users/models.py
@@ -5,9 +5,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile.jpg',
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics',
                               height_field=None, width_field=None, max_length=None)
 
     def __str__(self):
-        return f'{self.user.username} Profile'                         
-
+        return f'{self.user.username} Profile'
