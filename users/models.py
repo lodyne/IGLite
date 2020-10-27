3f34b9e5424dd958c194750cf546aaf2a72081c5
lodyne <lodgmtui@gmail.com>
2020-10-27 17:03:23 +0300

    continue user profile functionality

--- users/models.py
+++ users/models.py
@@ -8,12 +8,23 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile.jpg',
                               height_field=None, width_field=None, max_length=None)
+    name = models.CharField(max_length=50, default='DEFAULT VALUE')
+    website = models.URLField(max_length=50, default='DEFAULT VALUE')
+    bio = models.TextField(max_length=500, default='DEFAULT VALUE')
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    gender = models.CharField(
+        max_length=50, choices=GENDER_CHOICES, default='DEFAULT VALUE')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
